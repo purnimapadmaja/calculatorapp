d2fe9d20889e7e8448729455f9b51e59ea9625d8
purnima <96984043+purnimapadmaja@users.noreply.github.com>
2023-09-06 16:59:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function clickbtn(value){
+    document.getElementById("calvalue").value+=value
+}
+function clearScreen(){
+    document.getElementById("calvalue").value=" "
+}
+function sequal(){
+    let equl= document.getElementById("calvalue").value
+    let equals=eval(equl)
+    document.getElementById("calvalue").value=equals
+    return equals
 
+}
